46d7ef30b8804cd9e3687efb43c0b38053bb4dff
Tenifoladev <sikiru@imanimed.com>
2026-06-30 12:36:16 +0100

    Add tenny_python.py

---
+++ tenny_python.py
@@ -0,0 +1,17 @@
+#print ('My name', 26, 'Is Adebayo')
+import random
+
+
+x=3
+y='my name is never Sikiru'
+#print(x, y)
+#print(y)
+print("sikiru" in y)
+
+# for x in 'banana':
+#     print(x)
+# slicing a string 
+b = 'banana'
+print(b[1:3])
+print(b[1:]) # from index 1 to the end
+print(b[:3]) # from the start to index 3
