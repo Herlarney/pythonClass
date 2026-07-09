24cdeaf3d798eb2646b663416193f7faf9ea0e1a
Tenifoladev <sikiru@imanimed.com>
2026-07-09 18:22:17 +0100

    python dictionary started

---
+++ tuples.py
@@ -0,0 +1,44 @@
+# Tuple
+# Tuples are used to store multiple items in a single variable.
+
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+# A tuple is a collection which is ordered and unchangeable.
+
+# Tuples are written with round brackets.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+# Create Tuple With One Item
+# To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
+
+# Example
+# One item tuple, remember the comma:
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+# Change Tuple Values
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+
+# But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+# Convert the tuple into a list to be able to change it:
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+# Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+
+# Example
+# Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
